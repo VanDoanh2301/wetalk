33c7479b204a5bf06df3c0cbafa6b40b7600fd88
Your Name <doanhtv2759@gmail.com>
2023-12-04 17:54:56 +0700

    commit to home fragment

--- app/src/main/java/com/example/wetalk/ui/fragment/TalkHomeFragment.kt
+++ app/src/main/java/com/example/wetalk/ui/fragment/TalkHomeFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 import com.example.wetalk.R
 import com.example.wetalk.databinding.FragmentTalkHomeBinding
+import com.example.wetalk.ui.activity.MainActivity
 
 /**
  * A simple [Fragment] subclass.
@@ -37,9 +40,31 @@ class TalkHomeFragment : Fragment() {
 
     private fun initBottomNavigation() {
         binding.fgHome.apply {
-            pagerMain.adapter(FragmentStateAdapter())
+            pagerMain.adapter = object : FragmentStatePagerAdapter(
+                (activity as MainActivity).supportFragmentManager,
+                BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+            ) {
+                override fun getItem(position: Int): Fragment {
+                    // Trả về fragment tại vị trí position
+                    return when (position) {
+                        0 -> (activity as MainActivity).supportFragmentManager.findFragmentById(R.id.talkStudyFragment)
+                            ?: TalkChatFragment()
+
+                        1 -> (activity as MainActivity).supportFragmentManager.findFragmentById(R.id.talkStudyFragment)
+                            ?: TalkStudyFragment()
+
+                        2 -> (activity as MainActivity).supportFragmentManager.findFragmentById(R.id.talkProfileFragment)
+                            ?: TalkProfileFragment()
+
+                        else -> throw IllegalArgumentException("Invalid position: $position")
+                    }
+                }
+
+                override fun getCount(): Int {
+                    return 3
+                }
+            }
         }
     }
-
 
 }
